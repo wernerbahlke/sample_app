99d3d67f90b171dc1fd4aa64c1507013c9aae4b4
Werner Bahlke <werner.bahlke@gmail.com>
2011-05-12 17:43:03 -0700

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -104,10 +104,16 @@ describe UsersController do
         end.should change(User, :count).by(1)
       end
 
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
+
       it "should redirect to the user show page" do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
       end    
+
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
