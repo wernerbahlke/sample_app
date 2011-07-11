988bfe548dab0d4ac8809a29e750e09db8a838ee
Werner Bahlke <werner.bahlke@gmail.com>
2011-07-11 14:31:32 -0700

    Refactored following and followers action 12.5.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,18 +77,19 @@ end
   end
 
   def following
-    @title = "Following"
-    @user = User.find(params[:id])
-    @users = @user.following.paginate(:page => params[:page])
-    render 'show_follow'
+    show_follow(:following)
   end
 
   def followers
-    @title = "Followers"
-    @user = User.find(params[:id])
-    @users = @user.followers.paginate(:page => params[:page])
-    render 'show_follow'
+    show_follow(:followers)
   end
+
+  def show_follow(action)
+    @title = action.to_s.capitalize
+    @user = User.find(params[:id])
+    @users = @user.send(action).paginate(:page => params[:page])
+    render 'show_follow'
+   end
 
   private
 
